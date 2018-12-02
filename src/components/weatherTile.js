1ced5c965bd34181d04d4282a956727335141946
Sumit Jangir <sumitjangir.jecrc@gmail.com>
2018-12-02 12:11:00 +0530

    some changes

--- src/components/weatherTile.js
+++ src/components/weatherTile.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Thumbnail, Col } from "react-bootstrap";
+import { Col } from "react-bootstrap";
 import "../App.css";
 
 class WeatherTile extends Component {
@@ -11,27 +11,74 @@ class WeatherTile extends Component {
       this.props.weatherDetail.weather[0].icon +
       ".png";
     let low_temp = parseFloat(this.props.weatherDetail.main.temp_min);
-    let high_temp = parseFloat(this.props.weatherDetail.main.temp_min);
+    let high_temp = parseFloat(this.props.weatherDetail.main.temp_max);
 
     low_temp -= kelvin;
     high_temp -= kelvin;
 
+    const monthNames = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December"
+    ];
+
     return (
       <Col className="weather-tile">
         <div>
-          <h3>
-            {date.getDate()}/{date.getMonth()}/{date.getFullYear()}
-          </h3>
+          <p style={{ paddingTop: "5px" }}>
+            <a
+              href="javascript:void(0);"
+              style={{
+                textDecoration: "none",
+                fontWeight: "bold",
+                fontSize: "large"
+              }}
+            >
+              {date.getDate()} {monthNames[date.getMonth()]}
+            </a>
+          </p>
           <img
             src={icon}
+            alt={this.props.weatherDetail.weather[0].description}
             style={{ width: "120px", height: "120px", display: "inline-block" }}
           />
-          <p>
-            <strong>{Math.round(low_temp, 2)}&#176;</strong>/
-            <span>{Math.round(high_temp, 2)}&#176;</span>
-          </p>
-          <p>Humidity : {this.props.weatherDetail.humidity}</p>
-          <p>Weather : {this.props.weatherDetail.weather[0].description}</p>
+          <div
+            style={{
+              textAlign: "left",
+              paddingLeft: "10px",
+              wordBreak: "break-word"
+            }}
+          >
+            <p>
+              <strong>{Math.round(low_temp, 2)}&#176;</strong>/
+              <span>{Math.round(high_temp, 2)}&#176;</span>
+            </p>
+            <p
+              style={{
+                wordBreak: "break-word"
+              }}
+            >
+              <span>Humidity : </span>
+              <span>{this.props.weatherDetail.main.humidity}</span>
+            </p>
+            <p
+              style={{
+                wordBreak: "break-word"
+              }}
+            >
+              <span>Weather : </span>
+              <span>{this.props.weatherDetail.weather[0].description}</span>
+            </p>
+          </div>
         </div>
       </Col>
     );
