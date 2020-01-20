d936146b23c2df5f60169d722e4421b490b9a1c5
Sumit Jangir <sumj@saxobank.com>
2020-01-20 12:16:05 +0530

    build

---
+++ build/precache-manifest.a466b9eed031c8827556cfbf554c2b3f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c81a06d30cfc802c0bb4",
+    "url": "/static/js/main.c81a06d3.chunk.js"
+  },
+  {
+    "revision": "550c7419f8873abb0471",
+    "url": "/static/js/1.550c7419.chunk.js"
+  },
+  {
+    "revision": "c81a06d30cfc802c0bb4",
+    "url": "/static/css/main.6d7a3543.chunk.css"
+  },
+  {
+    "revision": "e24339a2012cf15454f98811f25e639f",
+    "url": "/index.html"
+  }
+];
